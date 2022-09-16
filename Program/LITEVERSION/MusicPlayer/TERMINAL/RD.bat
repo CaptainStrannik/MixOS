84aeb9b17f8cdc4d176a3c62df1b5f88653876f7
Lepekhin Sergey <47190179+CaptainStrannik@users.noreply.github.com>
2022-09-16 22:54:04 +0300

    Liteversion MixOs 2.3

---
+++ Program/LITEVERSION/MusicPlayer/TERMINAL/RD.bat
@@ -0,0 +1,66 @@
+@echo off
+echo Removing %SystemRoot%\addins [..........]    10
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [-.........]    09
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [--........]    08
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [---.......]    07
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [----......]    06
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [-----.....]    05
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [------....]    04
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [-------...]    03
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [--------..]    02
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\addins [---------.]    01
+TIMEOUT /T 1 /NOBREAK>nul
+echo %SystemRoot%\addins was removed
+TIMEOUT /T 3 /NOBREAK>nul
+echo Removing %SystemRoot%\system [..........]     10
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [-.........]     09
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [--........]     08
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [---.......]     07
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [----......]     06
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [-----.....]     05
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [------....]	   04	
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [-------...]     03
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [--------..]     02
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removing %SystemRoot%\system [---------.]	   01	
+TIMEOUT /T 1 /NOBREAK>nul
+echo Removed %SystemRoot%\system
+TIMEOUT /T 3 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [..........]     10
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [-.........]     09
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [--........]     08
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [---.......]     07
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [----......]     06
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [-----.....]     05
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [------....]     04	
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [-------...]     03
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [--------..]     02
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removing %SystemRoot%\system32 [---------.]     01	
+TIMEOUT /T 2 /NOBREAK>nul
+echo Removed %SystemRoot%\system32
